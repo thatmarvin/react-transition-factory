8d36202f197f7319bc94fac33d113adbafd1023c
Setsun <seanjeluo@gmail.com>
2018-04-10 21:29:33 -0400

    in-prog

---
+++ packages/components/src/group/ReplaceTransitionGroup.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ReplaceTransitionGroup extends React.Component {
+  constructor() {
+    this.state = {
+
+    }
+  }
+
+  render() {
+    const {
+      children,
+    } = this.props;
+
+    return (
+    );
+  }
+}
---
+++ packages/components/src/group/StaggeredTransitionGroup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class StaggeredTransitionGroup extends React.Component {
+  constructor() {
+    this.state = {
+
+    }
+  }
+
+  render() {
+    const {
+      stagger,
+      children,
+    } = this.props;
+
+    return (
+    );
+  }
+}
